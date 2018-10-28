5117aac05d3f65ccf17b8f8c7ed86b9e90586a6f
George <george.barnett.121@gmail.com>
2018-10-28 17:04:13 +0000

    Renaming Folders

--- 1 Data processing/convert.py
+++ 1 Data processing/convert.py
@@ -1,5 +1,19 @@
 import numpy as np
 
+'''
+GENERAL DESCRIPTION:
+	Each file converts 2 ubytes files into one hdf5 file containg two datasets: 'images' and 'labels'
+
+COMMAND LINE ARGUMENTS:
+	FILE_NAME INPUT_IMAGES INPUT_LABELS OUTPUT_DIRECTORY
+
+
+FILES:
+	No_Processing : does not processing on the dataset, just converts the ubytes into hdf5
+
+'''
+
+
 def main(run_dir,input_dir_images,input_dir_labels,output_dir):
 
     # get np array from ubytes
